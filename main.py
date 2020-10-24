bb9f75af53370451766d07fa7e301792230a8415
UCCMOL <aurelly321@gmail.com>
2020-10-24 16:44:39 +0800

    read message and write message success

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
-
+import dbm
+import re
 import discord
 from dotenv import load_dotenv
 
@@ -9,7 +10,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -20,5 +20,14 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
-
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    db = dbm.open('lineageBossTimer','c')
+    print(message.content)
+    db['save_word'] = message.content
+    await message.channel.send(str(db['save_word'], "utf-8"))
+    db.close()
+    
 client.run(TOKEN)

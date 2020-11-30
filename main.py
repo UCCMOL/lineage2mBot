fade63a94d293957e77099918527fca2c493f770
Jiong <aurelly321@gmail.com>
2020-11-30 10:25:25 +0000

    i'm not sure

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from collections import OrderedDict 
 from operator import getitem 
-
+import keep_alive
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 SETTING_CHANNEL_ID = os.getenv('DISCORD_SETTING_CHANNEL_ID')
-UTC_PLUS  = 8
+
+UTC_PLUS  = 9
 #regex = r"([\.!])([\w\p{Hangul}]+)\s*([\w\p{Hangul}]+)?\s*([\w\p{Hangul} \.]+)?"
 regex = r"([\.!])([\w\p{Hangul}]+)\s*([\w\p{Hangul}]+)?\s*([\w\p{Hangul}\.]+)? *([\+])? *([\w\p{Hangul} \.]+)?"
 def myconverter(o):
@@ -50,6 +51,7 @@ nickname = {
 "브" : "브래카",
 "메" : "메두사",
 "릴" : "블랙릴리",
+"릴리" : "블랙릴리",
 "베" : "베히모스",
 "드비" : "드래곤비스트",
 "카" : "카탄",
@@ -96,30 +98,32 @@ async def check_boss_time():
     else:
         configs = {"boss" : {}}
     now = await get_current_time()
+    print("do event every minutes start",now)
     for key,value in configs["boss"].items():
         boss_last_time = datetime.datetime.strptime(configs["boss"][key]["last_time"],"%H%M")
-        boss_next_time = boss_last_time + timedelta(hours=float(configs["boss"][key]["reborn_time"]))
+        boss_next_time = datetime.datetime.strptime(configs["boss"][key]["new_time"],"%H%M")
         boss_next_time_5_min_before = boss_next_time - timedelta(minutes=5)
         boss_next_time_4_min_before = boss_next_time - timedelta(minutes=4)
         boss_next_time_1_min_before = boss_next_time - timedelta(minutes=1)
-        boss_next_time_30_min_aftre = boss_next_time + timedelta(minutes=30)
-        if boss_next_time_5_min_before <= now and now < boss_next_time_4_min_before:
+        boss_next_time_30_min_after = boss_next_time + timedelta(minutes=30)
+        if boss_next_time_5_min_before == now:
             response = "```\n"
             #히실로메 - 격전의 평원에서 5분 안에 리젠 됩니다! 예상 젠 시간 : 19:38
             response = response + str(key) + " - " + str(value["place"]) + "에서 " + str(5) + "분 안에 리젠 됩니다! 예상 젠시간 :" + str(boss_next_time.strftime("%H%M")) + "\n"
             response = response + "```"
             await channel.send(response)
             print(5,key)
-        if boss_next_time_1_min_before <= now and now < boss_next_time:
+        if boss_next_time_1_min_before == now:
             response = "```\n"
             #히실로메 - 격전의 평원에서 1분 안에 리젠 됩니다! 예상 젠 시간 : 19:38
             response = response + str(key) + " - " + str(value["place"]) + "에서 " + str(1) + "분 안에 리젠 됩니다! 예상 젠시간 :" + str(boss_next_time.strftime("%H%M")) + "\n"
             response = response + "```"
             await channel.send(response)
             print(1,key)
-        if boss_next_time_30_min_aftre == now:
-            boss_unreborn(configs,channel,key)
+        if boss_next_time_30_min_after == now:
+            await boss_unreborn(configs,channel,key)
     db.close()
+    print("do event every minutes end")
 @check_boss_time.before_loop
 async def before_printer():
     print('waiting...')
@@ -145,7 +149,6 @@ async def on_message(message):
         return
     if(str(message.channel.id) != str(CHANNEL_ID) and str(message.channel.id) != str(SETTING_CHANNEL_ID)):
         return
-    print(message)
     db = dbm.open('lineageBossTimer','c')
     commands = message.content;
     if "lineage2m" in db:
@@ -165,7 +168,7 @@ async def show_boss_messages(configs,channel,boss_name = None):
         if boss_name in configs["boss"]:
             boss_last_time = datetime.datetime.strptime(configs["boss"][boss_name]["last_time"],"%H%M")
             boss_next_time = boss_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
-            response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(boss_name) + " " + str(configs["boss"][boss_name]["place"]) + " " + str(configs["boss"][boss_name]["reborn_time"])
+            response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(boss_name) + " " + str(configs["boss"][boss_name]["place"]) + " 리센시간" + str(configs["boss"][boss_name]["reborn_time"]) + " "
             if configs["boss"][boss_name]["unborn_times"] > 0:
                 response = response + "멍:" + str(configs["boss"][boss_name]["unborn_times"]) + "회 "
             response = response + str(configs["boss"][boss_name]["messages"]) + "\n"
@@ -173,14 +176,13 @@ async def show_boss_messages(configs,channel,boss_name = None):
         res = sorted(configs["boss"].items(), key = lambda x: x[1]['new_time']) 
         now = await get_current_time()
         now_time_in_int = int(now.strftime("%H%M"))
-        print(res)
         for key,value in res:
             boss_new_time = datetime.datetime.strptime(configs["boss"][key]["new_time"],"%H%M")
             boss_new_time_in_int = int(boss_new_time.strftime("%H%M"))
-            if(boss_new_time_in_int > now_time_in_int):
+            if(boss_new_time_in_int > now_time_in_int and boss_new_time_in_int < 2400):
               boss_last_time = datetime.datetime.strptime(configs["boss"][key]["last_time"],"%H%M")
               boss_next_time = boss_last_time + timedelta(hours=float(configs["boss"][key]["reborn_time"]))
-              response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(key) + " " + " " + str(configs["boss"][key]["place"]) + " " + str(configs["boss"][key]["reborn_time"])
+              response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(key) + " " + " " + str(configs["boss"][key]["place"]) + " "
               if configs["boss"][key]["unborn_times"] > 0:
                   response = response + "멍:" + str(configs["boss"][key]["unborn_times"]) + "회 "
               response = response + str(value["messages"]) + "\n"
@@ -190,7 +192,7 @@ async def show_boss_messages(configs,channel,boss_name = None):
             if(boss_new_time_in_int <= now_time_in_int):
               boss_last_time = datetime.datetime.strptime(configs["boss"][key]["last_time"],"%H%M")
               boss_next_time = boss_last_time + timedelta(hours=float(configs["boss"][key]["reborn_time"]))
-              response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(key) + " " + " " + str(configs["boss"][key]["place"]) + " " + str(configs["boss"][key]["reborn_time"])
+              response = response + str(boss_last_time.strftime("%H:%M")) + " -> " + str(boss_next_time.strftime("%H:%M")) + " " + str(key) + " " + " " + str(configs["boss"][key]["place"]) + " "
               if configs["boss"][key]["unborn_times"] > 0:
                   response = response + "멍:" + str(configs["boss"][key]["unborn_times"]) + "회 "
               response = response + str(value["messages"]) + "\n"
@@ -204,6 +206,9 @@ async def update_boss_messages(configs,channel,boss_name = None,target = None , 
     if boss_name in configs["boss"]:
         if target == "리젠시간":
             configs["boss"][boss_name]["reborn_time"] = float(new_setting)
+            boss_last_time = datetime.datetime.strptime(configs["boss"][boss_name]["last_time"],"%H%M")
+            boss_new_time = boss_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
+            configs["boss"][boss_name]["new_time"] = boss_new_time.strftime("%H%M")
         elif target == "지역":
             configs["boss"][boss_name]["place"] = str(new_setting)
         await show_boss_messages(configs,channel,boss_name)
@@ -245,7 +250,6 @@ async def delete_boss_messages(configs,channel,boss_name = None):
         await channel.send(response)
 async def randompick(configs,channel,pick_num = 0,peoples = ""):
     people_array = peoples.split(' ')
-    print(people_array)
     outcome = random.sample(people_array,k=int(pick_num))
     response = "```\n뽑기 결과\n"
     response = response + str(outcome)
@@ -260,25 +264,26 @@ async def kill_boss_messages(configs,channel,boss_name,kill_time = None , messag
     if boss_name in nickname:
         boss_name = nickname[boss_name]
     if boss_name in configs["boss"]:
-        boss_last_time = datetime.datetime.strptime(configs["boss"][boss_name]["last_time"],"%H%M")
-        boss_last_time = boss_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
-        boss_new_time = boss_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
-        configs["boss"][boss_name]["last_time"] = boss_last_time.strftime("%H%M")
-        configs["boss"][boss_name]["new_time"] = boss_new_time.strftime("%H%M")
         configs["boss"][boss_name]["unborn_times"] = 0
         if messages != None:
             configs["boss"][boss_name]["messages"] = messages
         if kill_time != None:
             if re.match(r"[0-9][0-9][0-9][0-9]?",kill_time):    
                 new_last_time = datetime.datetime.strptime(kill_time,"%H%M")
-                boss_new_time = boss_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
+                boss_new_time = new_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
                 configs["boss"][boss_name]["last_time"] = new_last_time.strftime("%H%M")
                 configs["boss"][boss_name]["new_time"] = boss_new_time.strftime("%H%M")
+        else:
+            new_last_time = await get_current_time()
+            boss_new_time = new_last_time + timedelta(hours=float(configs["boss"][boss_name]["reborn_time"]))
+            configs["boss"][boss_name]["last_time"] = new_last_time.strftime("%H%M")
+            configs["boss"][boss_name]["new_time"] = boss_new_time.strftime("%H%M")
+        
+
         await show_boss_messages(configs,channel,boss_name) 
 async def do_commands(matches,configs,channel):
     print(matches)
     for matchNum, match in enumerate(matches, start=1):
-        print(len(match.groups()))
         print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
@@ -287,7 +292,6 @@ async def do_commands(matches,configs,channel):
         if len(match.groups()) > 1:
             if match.group(1) == ".":
                 if match.group(2) == "보스":
-                    print("in show_boss")
                     await show_boss_messages(configs,channel,match.group(3))
                 elif match.group(2) == "컷":
                     await kill_boss_messages(configs,channel,match.group(3),kill_time = match.group(4),messages = match.group(6))
@@ -302,4 +306,6 @@ async def do_commands(matches,configs,channel):
                     await create_boss_messages(configs,channel,boss_name = match.group(3),reborn_time = match.group(4),place = match.group(6))
                 elif match.group(2) == "삭제":
                     await delete_boss_messages(configs,channel,boss_name = match.group(3))
+
+keep_alive.keep_alive()
 client.run(TOKEN)
